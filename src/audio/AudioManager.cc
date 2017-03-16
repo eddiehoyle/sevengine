1086886bf163656ae552acb134941986dcba5323
Eddie <eddiepie@gmail.com>
2017-03-16 22:06:24 +1300

    tidying up audio manager

--- src/audio/AudioManager.cc
+++ src/audio/AudioManager.cc
@@ -13,6 +13,9 @@
 #include <stdlib.h>
 
 #include <iostream>
+#include <vector>
+
+static const ALuint kMaxSources = 32;
 
 void checkALErrors( const char* errLocation ) {
     ALenum err = alGetError();
@@ -43,9 +46,18 @@ AudioManager::AudioManager()
         : m_device( NULL ),
           m_context( NULL ) {
 
+    // Get default device
     m_device = alcOpenDevice( NULL );
+    checkALErrors( "alcOpenDevice()" );
+
+    // Create context
     m_context = alcCreateContext( m_device, NULL );
+    checkALErrors( "alcOpenDevice()" );
+
+    //
     alcMakeContextCurrent( m_context );
+    checkALErrors( "alcOpenDevice()" );
+
 }
 
 void AudioManager::stop() {
@@ -54,9 +66,9 @@ void AudioManager::stop() {
 
 void AudioManager::play( const std::string& path ) {
 
-    // Generate buffers
-    alGenBuffers( 1, &m_buffer );
-    checkALErrors( "alGenBuffers" );
+//    // Generate buffers
+//    alGenBuffers( 1, &m_buffer );
+//    checkALErrors( "alGenBuffers" );
 
     FILE* file = fopen( path.c_str(), "rb" );
     if (!file) {
@@ -75,22 +87,25 @@ void AudioManager::play( const std::string& path ) {
         printf( "ERROR bad format\n" );
     }
 
+    std::cerr << "dataSize: " << header->dataSize << std::endl;
+
     char* buffer = ( char* )malloc( header->dataSize );
     size_t datasize = fread( buffer, header->dataSize, 1, file );
     fclose(file);
 
-
     ALuint format = 0;
     switch ( header->bitsPerSample ) {
-        case 8: format = ( header->channels == 1 ) ? AL_FORMAT_MONO8
-                                                   : AL_FORMAT_STEREO8;
+        case 8: format = ( header->channels == 1 ) ? AL_FORMAT_MONO8 : AL_FORMAT_STEREO8;
             break;
-        case 16: format = ( header->channels == 1 ) ? AL_FORMAT_MONO16
-                                                    : AL_FORMAT_STEREO16;
+        case 16: format = ( header->channels == 1 ) ? AL_FORMAT_MONO16 : AL_FORMAT_STEREO16;
             break;
         default: printf( "ERROR mono/stereo bad format\n");
             break;
     }
+
+    // Generate buffers
+    alGenBuffers( 1, &m_buffer );
+    checkALErrors( "alGenBuffers" );
 
     alBufferData( m_buffer, format, buffer, header->dataSize, header->samplesPerSec );
     checkALErrors( "alBufferData" );
@@ -106,5 +121,153 @@ void AudioManager::play( const std::string& path ) {
     alSourcei( m_source, AL_LOOPING, AL_FALSE );
 
     alSourcePlay( m_source );
+}
+
+
+// ------------------------------------------------------------------------------------------------------------ //
+
+
+
+AudioSourceWav::AudioSourceWav( const std::string& path )
+          : m_buffer( 0 ) {
+
+    // Open file
+    FILE* file = fopen( path.c_str(), "rb" );
+    if ( !file ) {
+        printf( "ERROR reading file\n" );
+    }
+
+    // Read file into header
+    m_header = new WavHeader();
+    if ( !fread( m_header, sizeof( WavHeader ), 1, file ) ) {
+        printf( "ERROR bad file\n" );
+    }
+
+    // Check header is valid wav
+    if ( ( strcmp( "RIFF", m_header->riff ) ||
+           strcmp( "WAVE", m_header->wave ) ||
+           strcmp( "fmt ", m_header->fmt ) ||
+           strcmp( "data", m_header->data ) ) ) {
+        printf( "ERROR bad format\n" );
+    }
+
+    // Read file into buffer
+    char* buffer = new char[m_header->dataSize];
+    fread( buffer, m_header->dataSize, 1, file );
+    fclose( file );
+
+    // Generate buffers
+    alGenBuffers( 1, &m_buffer );
+    alBufferData( m_buffer, getChannels(), buffer, m_header->dataSize, m_header->samplesPerSec );
+
+    // Cleanup
+    delete[] buffer;
+}
+
+AudioSourceWav::~AudioSourceWav() {
+    delete m_header;
+}
+
+ALuint AudioSourceWav::getBuffer() const {
+    return m_buffer;
+}
+
+ALuint AudioSourceWav::getChannels() {
+
+    ALuint format = 0;
+    switch ( m_header->bitsPerSample ) {
+        case 8:
+            format = ( m_header->channels == 1 ) ? AL_FORMAT_MONO8 : AL_FORMAT_STEREO8;
+            break;
+        case 16:
+            format = ( m_header->channels == 1 ) ? AL_FORMAT_MONO16 : AL_FORMAT_STEREO16;
+            break;
+        default:
+            printf( "ERROR mono/stereo bad format\n");
+            break;
+    }
+    return format;
+}
+
+
+AudioManager2* AudioManager2::m_instance = 0;
+
+AudioManager2* AudioManager2::instance() {
+    if ( m_instance == 0 ) {
+        m_instance = new AudioManager2();
+    }
+    return m_instance;
+}
+
+AudioManager2::AudioManager2()
+        : m_device( NULL ),
+          m_context( NULL ),
+          m_pool() {
+
+    // Get default device
+    m_device = alcOpenDevice( NULL );
+    m_context = alcCreateContext( m_device, NULL );
+    alcMakeContextCurrent( m_context );
+
+    // Generate sources
+    ALuint sources[ kMaxSources ];
+    alGenSources( kMaxSources, sources );
+
+    // Store in vector
+    m_pool.reserve( kMaxSources );
+    std::copy( sources, sources + kMaxSources, std::back_inserter( m_pool ) );
+}
+
+void AudioManager2::play( const AudioSourceWav& source ) {
+
+//    ALuint source = 0;
+//    acquire( &source );
+}
+
+void AudioManager2::stop( const AudioSourceWav& source ) {
+
+}
+
+void AudioManager2::acquire( ALuint* source ) {
+
+    typedef std::vector< ALuint >::const_iterator SourceIterator;
+    for ( SourceIterator iter = m_pool.begin();
+          iter != m_pool.end();
+          ++iter ) {
+
+        ALuint checkSource = *iter;
+
+        // Is this source active?
+        SourceIterator activeIter =
+                std::find( m_activeSources.begin(), m_activeSources.end(), checkSource );
+
+        // Found a free source
+        if ( activeIter != m_activeSources.end() ) {
+            m_activeSources.push_back( checkSource );
+            *source = checkSource;
+        }
+    }
+}
+
+void AudioManager2::restore( ALuint* source ) {
+
+    // Is this source active?
+    typedef std::vector< ALuint >::const_iterator SourceIterator;
+    SourceIterator activeIter = std::find( m_activeSources.begin(),
+                                           m_activeSources.end(),
+                                           *source );
+
+    // Found a free source
+    if ( activeIter != m_activeSources.end() ) {
+        m_activeSources.erase( activeIter );
+    }
+}
+
+
+AudioManager2::~AudioManager2() {
+
+    alcMakeContextCurrent( NULL );
+    alcDestroyContext( m_context );
+    alcCloseDevice( m_device );
 }
 
--- src/audio/AudioManager.hh
+++ src/audio/AudioManager.hh
@@ -10,6 +10,8 @@
 
 #include <OpenAL/al.h>
 #include <OpenAL/alc.h>
+#include <map>
+#include <vector>
 
 void checkALErrors( const char* errLocation );
 
@@ -18,6 +20,7 @@ class AudioManager {
 public:
     static AudioManager* instance();
 
+    // https://github.com/dilawar/sound/blob/master/include/wav-def.h#L47
     struct wave_header_t {
         char riff[4]; //'RIFF'
         unsigned int riffSize;
@@ -57,5 +60,106 @@ private:
 
 };
 
+/*
+ * AudioManager2::instance()->acquire();
+ */
+
+// https://github.com/dilawar/sound/blob/master/include/wav-def.h#L47
+
+
+class AudioSourceWav {
+
+    struct WavHeader {
+        char riff[4]; //'RIFF'
+        unsigned int riffSize;
+        char wave[4]; //'WAVE'
+        char fmt[4]; //'fmt '
+        unsigned int fmtSize;
+        unsigned short format;
+        unsigned short channels;
+        unsigned int samplesPerSec;
+        unsigned int bytesPerSec;
+        unsigned short blockAlign;
+        unsigned short bitsPerSample;
+        char data[4]; //'data'
+        unsigned int dataSize;
+    };
+
+public:
+    AudioSourceWav( const std::string& path );
+    ~AudioSourceWav();
+
+    ALuint getChannels();
+    ALuint getBuffer() const;
+
+private:
+    WavHeader* m_header;
+    ALuint m_buffer;
+
+};
+
+class AudioManager2 {
+
+    /*
+     * On iOS there is a maximum of 32 sound sources playing at one time.
+     * So at program start, I create 32 ALSources (the ALSourcePool),
+     * and when a new sound needs to play, it asks the Pool for a
+     * SoundSource to use.
+     */
+
+public:
+    static AudioManager2 *instance();
+    ~AudioManager2();
+
+    // https://github.com/dilawar/sound/blob/master/include/wav-def.h#L47
+    struct WaveHeader {
+        char riff[4]; //'RIFF'
+        unsigned int riffSize;
+        char wave[4]; //'WAVE'
+        char fmt[4]; //'fmt '
+        unsigned int fmtSize;
+        unsigned short format;
+        unsigned short channels;
+        unsigned int samplesPerSec;
+        unsigned int bytesPerSec;
+        unsigned short blockAlign;
+        unsigned short bitsPerSample;
+        char data[4]; //'data'
+        unsigned int dataSize;
+    };
+
+public:
+    void play( const AudioSourceWav& source );
+    void stop( const AudioSourceWav& source );
+
+private:
+
+    // Acquire a source from pool to bind to buffer
+    void acquire( ALuint* source );
+
+    // Restore a source to pool
+    void restore( ALuint* source );
+
+private:
+    static AudioManager2* m_instance;
+
+    glm::vec2 m_position;
+    glm::vec2 m_velocity;
+
+    ALCdevice* m_device;
+    ALCcontext* m_context;
+
+    // Available sources
+    std::vector< ALuint > m_pool;
+
+    // Sources in use
+    std::vector< ALuint > m_activeSources;
+
+private:
+    AudioManager2();
+    AudioManager2( const AudioManager2& );
+    AudioManager2& operator=( const AudioManager2& );
+
+};
 
 #endif //SEVENGINE_AUDIOMANAGER_HH
